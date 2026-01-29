024a90d29c5c5ea577ae21aa88dbe8fcba15fd77
“Hadeel <3075042a@student.gla.ac.uk>
2026-01-29 12:39:46 +0000

    chapter3

--- rango/views.py
+++ rango/views.py
@@ -3,10 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    context_dict = {
-        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'
-    }
-    return render(request, 'rango/index.html', context=context_dict)
-    
-def about(request):
-    return render(request, 'rango/about.html')
+
+    return HttpResponse("Rango says hey there partner!")
+
+
