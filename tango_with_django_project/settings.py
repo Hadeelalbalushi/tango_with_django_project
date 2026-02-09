8f37a31cc6b24727d1331a4f99dab8c11139e979
“Hadeel <3075042a@student.gla.ac.uk>
2026-02-09 22:07:40 +0000

    chapter9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -131,3 +131,12 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = MEDIA_DIR
+
+
+PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+        'django.contrib.auth.hashers.BCryptPasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+LOGIN_URL = 'rango:login'
